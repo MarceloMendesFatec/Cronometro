d3af206b6f2b75cc2e0cbab2e44807e3c01e7eb9
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2024-02-23 14:27:45 -0300

    done

--- App.js
+++ App.js
@@ -3,23 +3,42 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 const App = () => {
-  const [number, setNumber] = useState(0);
+  
+  const [btnText, setBtnText] = useState("Start");
 
   //variavel do relogio
+  const [number, setNumber] = useState(0);
   const [timer, setTimer] = useState(null);
+  const [lastTime, setLastTime] = useState(0);
 
   //função para iniciar o cronometro
   start = () => {
+    //verificar se o timer está rodando e para ele
     if (timer !== null) {
       clearInterval(timer);
       setTimer(null);
+      setBtnText("Start");
     } else {
-      setTimer( setInterval(() => {setNumber((prevNumber) => prevNumber + 0.1)}, 100));
+      //iniciar o cronometro quando for nulo
+      setTimer(
+        setInterval(() => {
+          setNumber((prevNumber) => prevNumber + 0.1);
+        }, 100)
+      );
+      setBtnText("Stop");
     }
   };
 
   //função para limpar o cronometro
-  clear = () => {};
+  clear = () => {
+    if (timer !== null) {
+      clearInterval(timer);
+      setTimer(null);
+    }
+    setNumber(0);
+    setBtnText("Start");
+    setLastTime(number.toFixed(1));
+  };
 
   return (
     <View style={styles.container}>
@@ -29,11 +48,18 @@ const App = () => {
       {/* Botões */}
       <View style={styles.btnArea}>
         <TouchableOpacity style={styles.btn} onPress={start}>
-          <Text style={styles.btnText}>Start</Text>
+          <Text style={styles.btnText}>{btnText}</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.btn} onPress={clear}>
           <Text style={styles.btnText}>Clear</Text>
         </TouchableOpacity>
+      </View>
+
+      {/* Last Time */}
+      <View>
+        <Text style={styles.lastTime}>
+          {lastTime > 0 ? "Last time is : " + lastTime : ""}
+        </Text>
       </View>
     </View>
   );
@@ -77,5 +103,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     color: "#00aeef",
+  },
+  lastTime: {
+    marginTop: 80,
+    width: 200,
+    padding: 12,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
   },
 });
