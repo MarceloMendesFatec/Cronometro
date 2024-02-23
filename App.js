8d174cdc3d50a01c7046a26f495acd41a025aa3b
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2024-02-23 13:11:58 -0300

    logica do inicio e pause

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
-const  App = () => {
+const App = () => {
   const [number, setNumber] = useState(0);
- 
 
- 
-start = () => {
- 
-}
+  //variavel do relogio
+  const [timer, setTimer] = useState(null);
 
-clear = () => {
+  //função para iniciar o cronometro
+  start = () => {
+    if (timer !== null) {
+      clearInterval(timer);
+      setTimer(null);
+    } else {
+      setTimer( setInterval(() => {setNumber((prevNumber) => prevNumber + 0.1)}, 100));
+    }
+  };
 
-}
+  //função para limpar o cronometro
+  clear = () => {};
 
-  
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/cronometro.png')} style={styles.img} />
-      <Text style={styles.textTimer}>{number.toFixed(2)}</Text>
+      <Image source={require("./assets/cronometro.png")} style={styles.img} />
+      <Text style={styles.textTimer}>{number.toFixed(1)}</Text>
 
+      {/* Botões */}
       <View style={styles.btnArea}>
         <TouchableOpacity style={styles.btn} onPress={start}>
           <Text style={styles.btnText}>Start</Text>
@@ -31,44 +37,45 @@ clear = () => {
       </View>
     </View>
   );
-}
+};
 
 export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00aeef',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#00aeef",
+    alignItems: "center",
+    justifyContent: "center",
   },
   img: {
     width: 200,
     height: 250,
-    marginTop: -200
+    marginTop: -200,
   },
   textTimer: {
     fontSize: 50,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: -140,
-    color: '#fff'
+    color: "#fff",
   },
   btnArea: {
-    flexDirection: 'row',
-    marginTop: 70,
-    height: 40
+    flexDirection: "row",
+    marginTop: 100,
+    height: 40,
   },
   btn: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
     height: 40,
     margin: 17,
-    borderRadius: 9
+    borderRadius: 9,
   },
   btnText: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#00aeef'
-  }
+    fontWeight: "bold",
+    color: "#00aeef",
+  },
 });
